bfb99372b580950f1d2a3b7709afbf710c28f9ab
MANISH KAVAIYA <manishkavaiya2005@gmail.com>
2025-09-19 17:48:09 +0530

    PYTHON

--- nested/check four greatest.py
+++ nested/check four greatest.py
@@ -1,16 +1,39 @@
 # TODO find the greatest value in four number
-# !!! using the nested if 
+
+# !!! using the nested if
 
 n1 = int(input("Enter the value 1 : "))
 n2 = int(input("Enter the value 2 : "))
 n3 = int(input("Enter the value 3 : "))
 n4 = int(input("Enter the value 4 : "))
 
-if n1 > n2 and n1 > n3 and n1 > n4:
-    print("n1 is greatest")
-elif n2 > n3 and n3 > n4:
-    print("n2 is greatest")
-elif n3 > n4:
-    print('n3 is greatest')
+# if n1 > n2 and n1 > n3 and n1 > n4:
+#     print("n1 is greatest")
+# elif n2 > n3 and n3 > n4:
+#     print("n2 is greatest")
+# elif n3 > n4:
+#     print('n3 is greatest')
+# else:
+#     print("n4 is greatest")
+
+
+# ** new method try out
+
+if n1 > n2:
+    if n2 > n3:
+        if n3 > n4:
+            print("n1 is greatest")
+    else:
+        print('n3 is greatest')
+
+elif n2 > n3:
+    if n2 > n4:
+        print('n2 is greatest')
+    else:
+        print('n4 is greatest')
+
 else:
-    print("n4 is greatest")
+    if n3 > n4:
+        print("n3 is greatest")
+    else:
+        print('n4 is greatest')
