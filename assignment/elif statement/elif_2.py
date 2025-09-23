5ddf1d43ab3450af6e8c99d92e5fb74dce9e407f
MANISH KAVAIYA <manishkavaiya2005@gmail.com>
2025-09-23 11:18:54 +0530

    PYTHON

--- assignment/elif statement/elif_2.py
+++ assignment/elif statement/elif_2.py
@@ -3,5 +3,10 @@
 n  = int(input("Enter your age : "))
 
 if n < 13:
-    print("IT is a child")
-elif 13>n>15
+    print("you are child")
+elif 13<=n<=19:
+    print("you are teenager")
+elif n > 20:
+    print("you are adult")
+else:
+    print("you are older")
